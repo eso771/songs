dc9b5bc8b2e05e21786e8890c132f0e0a673299d
eko777e <replitserver445@gmail.com>
2026-03-14 21:27:07 +0400

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = int(os.environ.get("API_ID", "18482353"))
 
 API_HASH = os.environ.get("API_HASH", "9f7840b7015b359a49e142ce42decd71")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7887939438:AAGOEqFr0zC1c6MbYYmT13idY2THTmtfzx4")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8344045278:AAHchSQn8Te4i8XR4ZJXrNVhx698JN-l5fs")
 
 PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1003767974813"))
 
@@ -13,6 +13,6 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "8493825254"))
 
 LOG_GROUP_ID = int(os.environ.get("LOG_GROUP_ID", "-1003767974813"))
 
-BOT_ID = int(os.environ.get("BOT_ID", "7887939438")) 
+BOT_ID = int(os.environ.get("BOT_ID", "8344045278")) 
 
 LOG_FILE_NAME = "Songlogs.txt"
