0251626e2c89d3d9c446a6066f34f48417cf900f
eso771 <elsnmuradli17@gmail.com>
2026-03-14 20:24:20 +0400

    Add configuration settings for API and bot

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+
+API_ID = int(os.environ.get("API_ID", "18482353"))
+
+API_HASH = os.environ.get("API_HASH", "9f7840b7015b359a49e142ce42decd71")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8465309717:AAERLZfaxGPegJB6ADl44J0rtDzyszMFR-4")
+
+PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1003190914603"))
+
+OWNER_ID = int(os.environ.get("OWNER_ID", "7978943627"))
+
+LOG_GROUP_ID = int(os.environ.get("LOG_GROUP_ID", "-1003127023064"))
+
+BOT_ID = int(os.environ.get("BOT_ID", "8465309717")) 
+
+LOG_FILE_NAME = "Songlogs.txt"
