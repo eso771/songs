74f6232be439f0e90cb5f1a7f4f2eedf77797894
eso771 <elsnmuradli17@gmail.com>
2026-03-27 23:08:15 +0400

    Update mongo.py

--- Song/core/mongo.py
+++ Song/core/mongo.py
@@ -7,7 +7,7 @@ from Song.logger import LOGGER
 LOGGER.info("Connecting to your Mongo Database...")
 try:
     _mongo_async_ = AsyncIOMotorClient(Cinfig.MONGODB_URI)
-    mongodb = _mongo_async_.Inflex   # <-- burda "Inflex" əslində sənin DB adındır
+    mongodb = _mongo_async_.Inflex
     LOGGER.info("Connected to your Mongo Database.")
 except Exception as e:
     LOGGER.error(f"Failed to connect to your Mongo Database: {e}")
